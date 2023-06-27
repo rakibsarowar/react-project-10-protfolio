c203ebca9ece17e510861ed2bc1952b42530dac7
rakibsarowar <rakibsarowar@gmail.com>
2023-06-27 07:14:01 +0600

    working on nav linking with skills section

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
             <nav className='center nav'>
                 <ul className='nav__list'>
-                    <li className='nav__list-item'><a className='link link--nav' href="">Skills</a></li>
+                    <li className='nav__list-item'><a className='link link--nav' href="#skills">Skills</a></li>
                     <li className='nav__list-item'><a className='link link--nav' href="">Projects</a></li>
                     <li className='nav__list-item'><a className='link link--nav' href="">Contact</a></li>
                 </ul>
