20cecc11146f6af37115f086073b6439c9a1315a
rakibsarowar <rakibsarowar@gmail.com>
2023-06-27 08:15:12 +0600

    added about me section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from './Components/Header/Header';
 import './assets/App.css'
 import Hero from './Components/Hero/Hero';
 import Skills from './Components/Skills/Skills';
+import AboutMe from './Components/AboutMe/AboutMe';
 
 const App = () => {
   return (
     <div id='top' className='light app'>
       <Header></Header>
       <Hero></Hero>
+      <AboutMe></AboutMe>
       <Skills></Skills>
     </div>
   );
---
+++ src/Components/AboutMe/AboutMe.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './AboutMe.css'
+
+const AboutMe = () => {
+    return (
+        <div className='aboutMe'>
+            <p>MORE ABOUT ME</p>
+            <div className='main-text'>
+            <h2>Who loves to code, create, and conquer the digital realm. A web developer with a passion for turning ideas into captivating online experiences.</h2>
+            </div>
+        </div>
+    );
+};
+
+export default AboutMe;
