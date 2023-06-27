15762717b6335678a303ce9ca69ed32004da043c
rakibsarowar <rakibsarowar@gmail.com>
2023-06-27 11:23:04 +0600

    working with projects

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { projects } from '../../data';
 import uniqid from 'uniqid'
 import ProjectContainer from '../ProjectContainer/ProjectContainer';
+import './Projects.css'
 
 const Projects = () => {
     if (!projects.length) return null
 
     return (
-        <div>
+        <>
             <section id='projects' className='section projects'>
                 <h2 className='section__title'>Projects</h2>
 
@@ -17,7 +18,7 @@ const Projects = () => {
                     ))}
                 </div>
             </section>
-        </div>
+        </>
     );
 };
 
