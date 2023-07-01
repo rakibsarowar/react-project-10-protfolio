7904b82e6c5380b1377ad3442597a2041fa0eb17
rakibsarowar <rakibsarowar@gmail.com>
2023-07-01 15:07:25 +0600

    working on project section

--- src/data.jsx
+++ src/data.jsx
@@ -31,18 +31,18 @@ const header = {
       description:
         'In this the project, ensures the privacy of sensitive information by implementing private routing, allowing access only to authorized users.',
       stack: ['Tailwind', 'React', 'MongoDB'],
-      image:'https://i.ibb.co/hdRMj74/Project-03.jpg',
-      sourceCode: 'https://github.com',
-      livePreview: 'https://github.com',
+      image:'https://i.ibb.co/ZLvdFf7/project-2-1.jpg',
+      sourceCode: 'https://github.com/rakibsarowar/mern-project-02-toy-market',
+      livePreview: 'https://lego-world-6fdfc.web.app/',
     },
     {
       name: 'EASYMEAL RECIPES',
       description:
         'This project users can conveniently log in to the platform using their email credentials, Gmail accounts, or GitHub accounts and offering multiple authentication options.',
       stack: ['Bootstrap', 'Firebase', 'React'],
-      image:'https://i.ibb.co/hdRMj74/Project-03.jpg',
-      sourceCode: 'https://github.com',
-      livePreview: 'https://github.com',
+      image:'https://i.ibb.co/XFPk5zv/Project-01.jpg',
+      sourceCode: 'https://github.com/rakibsarowar/mern-project-01-Easy-Meals',
+      livePreview: 'https://easymeals-83bfe.web.app/',
     },
   ]
   
