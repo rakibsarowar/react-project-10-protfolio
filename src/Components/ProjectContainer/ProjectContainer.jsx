9bc37f22bdf4338e2f8520a7fc0f677be5e7b3e4
rakibsarowar <rakibsarowar@gmail.com>
2023-07-03 21:14:03 +0600

    update the icon

--- src/Components/ProjectContainer/ProjectContainer.jsx
+++ src/Components/ProjectContainer/ProjectContainer.jsx
@@ -1,50 +1,49 @@
 import React from 'react';
-import uniqid from 'uniqid'
-import './ProjectContainer.css'
-import GitHubIcon from '@material-ui/icons/GitHub'
-import LaunchIcon from '@material-ui/icons/Launch'
+import uniqid from 'uniqid';
+import './ProjectContainer.css';
+import { AiFillGithub, AiOutlineLink } from 'react-icons/ai';
 
 const ProjectContainer = ({ project }) => {
-    console.log(project.image)
-    return (
-        <>
-            <div className='project'>
-                <img className='project__image' src={project.image} alt="" />
-                <h3>{project.name}</h3>
+  console.log(project.image);
+  return (
+    <>
+      <div className='project'>
+        <img className='project__image' src={project.image} alt='' />
+        <h3>{project.name}</h3>
 
-                <p className='project__description'>{project.description}</p>
-                {project.stack && (
-                    <ul className='project__stack'>
-                        {project.stack.map((item) => (
-                            <li key={uniqid()} className='project__stack-item'>
-                                {item}
-                            </li>
-                        ))}
-                    </ul>
-                )}
+        <p className='project__description'>{project.description}</p>
+        {project.stack && (
+          <ul className='project__stack'>
+            {project.stack.map((item) => (
+              <li key={uniqid()} className='project__stack-item'>
+                {item}
+              </li>
+            ))}
+          </ul>
+        )}
 
-                {project.sourceCode && (
-                    <a
-                        href={project.sourceCode}
-                        aria-label='source code'
-                        className='link link--icon'
-                    >
-                        <GitHubIcon />
-                    </a>
-                )}
+        {project.sourceCode && (
+          <a
+            href={project.sourceCode}
+            aria-label='source code'
+            className='link link--icon'
+          >
+            <AiFillGithub />
+          </a>
+        )}
 
-                {project.livePreview && (
-                    <a
-                        href={project.livePreview}
-                        aria-label='live preview'
-                        className='link link--icon'
-                    >
-                       <LaunchIcon />
-                    </a>
-                )}
-            </div>
-        </>
-    );
+        {project.livePreview && (
+          <a
+            href={project.livePreview}
+            aria-label='live preview'
+            className='link link--icon'
+          >
+            <AiOutlineLink />
+          </a>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default ProjectContainer;
